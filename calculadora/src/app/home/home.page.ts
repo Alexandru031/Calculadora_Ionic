a9e7beb1c521ea28a16167960dc519b5989d59a7
Alexandru031 <alexnadrustanica33@gmail.com>
2024-01-22 23:52:16 +0100

    Calculadora Puntuable

--- calculadora/src/app/home/home.page.ts
+++ calculadora/src/app/home/home.page.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { SelectChangeEventDetail } from '@ionic/angular';
+import { IonSelectCustomEvent } from '@ionic/core';
+
+interface IonButtonElement extends HTMLElement {
+  expand: string;
+  strong: string;
+  fill: string;
+  size: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
+
 export class HomePage {
+
   result:string = '0';
-  constructor() {}
+  botonSeleccionar:string = "";
+  tamanoSeleccionar:string = "";
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  onChangeButton(event: { detail: { value: string; }; }) {
+    // event.detail.value contiene el valor seleccionado
+    this.botonSeleccionar = event.detail.value;
+    console.log("El boton seleccionado es:", this.botonSeleccionar)
+  }
+
+  onChangeSize(event: { detail: { value: string; }; }) {
+    // event.detail.value contains the selected value
+    this.tamanoSeleccionar = event.detail.value;
+    console.log("El tamaño seleccionado es:", this.tamanoSeleccionar);
+  
+    const boton = this.el.nativeElement.querySelector('#boton');
+    this.renderer.setAttribute(boton, "color", "dark");
+  }
 
   setResult(value:string){
     this.result = value;
@@ -35,4 +60,20 @@ export class HomePage {
   del() {
     this.setResult('0');
   }
+   
+  /* onLoad(): void { 
+    document.addEventListener("ionChange", this.setStyle);
+    this.setStyle();
+  }
+
+  setStyle(): void { 
+    /*document.querySelectorAll<HTMLIonButtonElement>("ion-content ion-button").forEach(function(b) {
+        b.expand = "block";
+        b.strong = true;
+        b.fill = document.getElementById("type")?.value;
+        b.size = document.getElementById("size")?.value;
+    });*/
+   /*} */
+
 }
+
